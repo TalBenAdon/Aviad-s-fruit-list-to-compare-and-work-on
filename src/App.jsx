4f5c53e6d046af373889933a599f578aae64ac88
Tal Ben Adon <talbenadon2@gmail.com>
2024-01-03 22:35:50 +0200

    deleted some unneeded imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import './App.css'
-import Effect from './Effect'
+
 import Layout from './Layout'
-import NotFound from './NotFound';
+
 
 import UserContext from './context/UserContext';
 import { Route, Routes } from 'react-router-dom';
